6a7adb6ba7d60c990ac9d5abac81e57ac86215d5
sanskaragrawal0608 <sanskaragrawal0608@gmail.com>
2026-05-14 15:34:27 +0530

    Fix production cookie settings

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -13,13 +13,12 @@ export const googleAuth = async (req,res) => {
             })
         }
         let token = await genToken(user._id)
-        res.cookie("token" , token , {
-            httpOnly:true,
-            secure:false,
-            sameSite:"strict",
-            maxAge:7 * 24 * 60 * 60 * 1000
-        })
-
+        res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+})
         return res.status(200).json(user)
 
 
